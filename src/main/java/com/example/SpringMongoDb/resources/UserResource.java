5c725ab47931823a6d6336d2eb22bda0a07c2bdc
yandiaszup <yan.dias@zup.com.br>
2018-09-18 15:21:22 -0300

    User Delete

--- src/main/java/com/example/SpringMongoDb/resources/UserResource.java
+++ src/main/java/com/example/SpringMongoDb/resources/UserResource.java
@@ -44,4 +44,11 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
