a448c3ad93eb196b5a2abeb785e5c22f4a4e30df
yandiaszup <yan.dias@zup.com.br>
2018-09-18 13:35:29 -0300

    Project created

---
+++ src/main/java/com/example/SpringMongoDb/SpringMongoDbApplication.java
@@ -0,0 +1,12 @@
+package com.example.SpringMongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDbApplication.class, args);
+	}
+}
