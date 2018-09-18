7f293fa5fc3bd6e0e6452be0c0ecc6a47063d4e0
yandiaszup <yan.dias@zup.com.br>
2018-09-18 17:35:06 -0300

    Referecing UserPosts

--- src/main/java/com/example/SpringMongoDb/config/Instantiation.java
+++ src/main/java/com/example/SpringMongoDb/config/Instantiation.java
@@ -31,17 +31,19 @@ public class Instantiation implements CommandLineRunner {
 
         userRepository.deleteAll(); //Deleta documentos da base de dados
         postRepository.deleteAll();
-        
+
         User maria = new User(null,"Maria Brown", "maria@gmail.com");
         User alex = new User(null,"Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1,post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1,post2));
+        userRepository.save(maria);
     }
 
 
