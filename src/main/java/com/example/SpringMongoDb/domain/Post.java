b87270a4448ddd2825e8898baa46bf55b71d4091
yandiaszup <yan.dias@zup.com.br>
2018-09-19 12:00:54 -0300

    Comments in posts (CommentDTO)

--- src/main/java/com/example/SpringMongoDb/domain/Post.java
+++ src/main/java/com/example/SpringMongoDb/domain/Post.java
@@ -1,11 +1,14 @@
 package com.example.SpringMongoDb.domain;
 
 import com.example.SpringMongoDb.dto.AuthorDTO;
+import com.example.SpringMongoDb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -18,6 +21,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post(){
 
@@ -69,6 +74,14 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
