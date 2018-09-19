f7c244c25c72b6044807e9ae777fa92b4da34b70
yandiaszup <yan.dias@zup.com.br>
2018-09-19 13:52:37 -0300

    Simple query with @Query

--- src/main/java/com/example/SpringMongoDb/repository/PostRepository.java
+++ src/main/java/com/example/SpringMongoDb/repository/PostRepository.java
@@ -2,12 +2,17 @@ package com.example.SpringMongoDb.repository;
 
 import com.example.SpringMongoDb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> { //Classe do repositorio e tipo do id do objeto
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTittle(String text);
+
 
     List<Post> findByTitleContainingIgnoreCase(String text); //IgnoreCase -> Ignora se a palavra tem letras maiusculas ou não
 }
